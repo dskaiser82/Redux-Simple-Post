3659ab401a5cd96a27b526ba4ab97064c1164fd5
“Dan <dkaiser@petrolad.com>
2018-07-10 11:46:36 -0700

    provider dobne

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './view/App';
-
+import postReducer from './reducers/postReducer'
 import { createStore } from 'redux';
 
+import { Provider } from 'react-redux';
 
-const store = createStore()
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//createStore always takes the reducer as an argument
+
+const store = createStore(postReducer);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+    </Provider>,
+  document.getElementById('root')
+ );
