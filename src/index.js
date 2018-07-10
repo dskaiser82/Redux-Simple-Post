e3600ae03a4a1ffec2b08eb410c93a6266e81e37
“Dan <dkaiser@petrolad.com>
2018-07-10 10:32:51 -0700

    readme

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './view/App';
 
+import { createStore } from 'redux';
+
+
+const store = createStore()
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
