876407af34140165376ebbd695eeb17bd576a2e8
“Dan <dkaiser@petrolad.com>
2018-07-10 18:09:15 -0700

    updated

--- src/view/Post.js
+++ src/view/Post.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 class Post extends Component {
   render() {
@@ -6,8 +7,12 @@ class Post extends Component {
     <div>
       <h2>{this.props.post.title}</h2>
       <p>{this.props.post.message}</p>
+      <button
+      onClick={()=> this.props.dispatch({type:'DELETE_POST', id:this.props.post.id})}>
+      DELETE</button>
+      <p>-----</p>
     </div>
   );
  }
 }
-export default Post;
+export default connect()(Post);
